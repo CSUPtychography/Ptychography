9473bce16b68026e27aba3ebb415bc6cb0ba8aa4
Robby <robbystk@rams.colostate.edu>
2016-11-05 13:36:45 -0600

    Use smaller pictures for the love of ram
    cameraman from
    http://breckon.eu/toby/fundipbook/materials/gallery/cameraman.png
    lena from https://www.cs.cmu.edu/~chuck/lennapg/len_std.jpg

--- mock-data/mock.m
+++ mock-data/mock.m
@@ -1,17 +1,21 @@
 %% create mock data to test phase retrieval algorithm
 
+%% parameters
+magnitude_file = 'cameraman.png';
+phase_file = 'lena.png';
 
 %% Import image data
 
-raw_image = imread('world.jpg');
+mag = imread(magnitude_file);
+phase = imread(phase_file);
 
-% split into magnitude and phase
-% use left half for magnitude and right half for phase
-% use green channel for good contrast and computational simplicity
+% check size and squareness
+% todo
+
 % normalize between 0 and 1 for magnitude,
 %   and -0.5 to 0.5 for phase
-mag = double(raw_image(:,1:12000,2)) / 255;
-phase = double(raw_image(:,12001:24000,2)) / 255 - 0.5;
+mag = double(mag) / 255;
+phase = double(phase) / 255 - 0.5;
 
 % pack into single phase image
 I = mag .* exp(1i * 2 * pi * phase);
