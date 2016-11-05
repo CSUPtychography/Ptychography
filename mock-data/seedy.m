f73aeb0b5c07d232e47c520a5d0d6cbd3592cd41
Robby <robbystk@rams.colostate.edu>
2016-11-05 14:34:22 -0600

    Corrected y seed function

--- mock-data/seedy.m
+++ mock-data/seedy.m
@@ -1,5 +1,5 @@
 function y = seedx(n)
 % creates y seed for nth spiral bit
     
-    y = [0:n n(ones(1,2*n-1)) n:-1:-n -n(ones(1,2*n-2)) -n:-1];
+    y = [0:n n(ones(1,2*n-1)) n:-1:-n -n(ones(1,2*n)) -n:-1];
 end % function
