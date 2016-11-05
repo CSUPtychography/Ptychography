f9854b12b6c511b48a305622dc08e0e365b12d3d
Robby <robbystk@rams.colostate.edu>
2016-11-05 14:19:54 -0600

    Extend seed function to x and y

--- mock-data/seed.m
+++
@@ -1,5 +0,0 @@
-function s = seed(n)
-% creates seed for nth spiral bit
-    
-    s = [0:n n(ones(1,2*n-2)) n:-1:-n -n(ones(1,2*n-1)) -n:-1];
-end % function
---
+++ mock-data/seedy.m
@@ -0,0 +1,5 @@
+function y = seedx(n)
+% creates y seed for nth spiral bit
+    
+    y = [0:n n(ones(1,2*n-1)) n:-1:-n -n(ones(1,2*n-2)) -n:-1];
+end % function
