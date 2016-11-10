a0692e6db5a1161b6a0797867cf0921d09bd11ab
Nicholas <nckbrwn@rams.colostate.edu>
2016-11-10 04:02:09 -0700

    Update mock-data debugging code

--- mock-data/mock.m
+++ mock-data/mock.m
@@ -1,10 +1,82 @@
-clear all;
-%% create mock data to test phase retrieval algorithm
-
-%% parameters
+% clear all;
+% %% create mock data to test phase retrieval algorithm
+% 
+% %% parameters
 magnitude_file = 'cameraman.png';
 phase_file = 'lena.png';
-r = 16;
+% r = 16;
+% 
+% %% Import image data
+% 
+% mag = imread(magnitude_file);
+% phase = imread(phase_file);
+% % check size and squareness
+% if size(mag) ~= size(phase)
+%     error('Images are not the same size');
+% end
+% 
+% % normalize between 0 and 1 for magnitude,
+% %   and -0.5 to 0.5 for phase
+% mag2 = double(mag) / 255;
+% phase2 = double(phase) / 255 - 0.5;
+% 
+% % pack into single phase image
+% I = mag2 .* exp(1i * 2 * pi * phase2);
+% 
+% % conserve memory ('cause we're gonna need it)
+% %% small image construction
+% % perform FFT on I
+% IF = fft2(I);
+% 
+% % Create Transfer Function
+% k = r+1;
+% TF = zeros(size(mag));
+% [m,n] = size(mag);
+% Images = cell(size(mag)/r);
+% imagesX=1;
+% while(k < m);
+% j=r+1;
+% imagesY=1;
+%     while (j < n);
+%     TF(k-r:k+r-1,j-r:j+r-1) = 255;
+%     Images{imagesX,imagesY} = TF.*IF;
+%     imagesY = imagesY+1;
+%     j = j+r;
+%     TF = zeros(size(mag));
+%     end
+% imagesX=imagesX+1;
+% k=k+r;
+% end
+% %% to be done with each segment
+% A = Images{2,2};
+% %trim each image
+% AC = A(A~=0);
+% %reshape each after trimming
+% ACR = reshape(AC,2*r,2*r);
+% %perform ifft2
+% blur = abs(ifft2(ACR));
+% %display image
+% figure ()
+% image(blur)
+% colormap(gray(256));
+% % problem here: will need to make this a circle instead of a rectangle,
+% % CIRCLE = insertShape(blank,'filled circle',[128 128 8]);   ?
+% % moving on...
+% 
+% % Measure Intensity
+% 
+% % Save small image in cell array
+
+% This function can be used to take any two images, and create images with
+% less resolution, to act as mock data.
+% magnitude_file is the file you'd like to act as the magnitude
+% phase_file is the file you'd like to act as the phase
+% r is the radius you'd like to use for the transfer function
+% a is the x coordinate of the location you'd like to have as the output
+% b is the y coordinate of the location you'd like to have as the output
+% try the following: 
+%       ImageMock('cameraman.png','lena.png',32,16,16);
+%% create mock data to test phase retrieval algorithm and ptychography Algorithm
 
 %% Import image data
 
@@ -14,57 +86,52 @@ phase = imread(phase_file);
 if size(mag) ~= size(phase)
     error('Images are not the same size');
 end
-
+%% Prepare imported data
 % normalize between 0 and 1 for magnitude,
-%   and -0.5 to 0.5 for phase
+% and -0.5 to 0.5 for phase
 mag2 = double(mag) / 255;
 phase2 = double(phase) / 255 - 0.5;
-
 % pack into single phase image
 I = mag2 .* exp(1i * 2 * pi * phase2);
-
 % conserve memory ('cause we're gonna need it)
 %% small image construction
 % perform FFT on I
-IF = fft2(I);
-
-% Create Transfer Function
+IF = fftshift(fft2(fftshift(I)));
+% preparing variables for transfer function
 k = r+1;
-TF = zeros(size(mag));
 [m,n] = size(mag);
-Images = cell(size(mag)/r);
+Images = cell((size(mag)/r)-1);
 imagesX=1;
 while(k < m);
 j=r+1;
 imagesY=1;
     while (j < n);
+        % preparing transfer function
+    TF = zeros(size(mag));
     TF(k-r:k+r-1,j-r:j+r-1) = 255;
+        % multiplying transfer function by FFt'd function, and assigning to
+        % cell array
     Images{imagesX,imagesY} = TF.*IF;
+        % counting through the image
     imagesY = imagesY+1;
     j = j+r;
-    TF = zeros(size(mag));
     end
 imagesX=imagesX+1;
 k=k+r;
 end
 %% to be done with each segment
-A = Images{2,2};
+A = Images{a,b};
 %trim each image
 AC = A(A~=0);
-%reshape each after trimming
+%reshape after trimming zeros
 ACR = reshape(AC,2*r,2*r);
-%perform ifft2
-blur = abs(ifft2(ACR));
+%perform ifft2 on fewer pixels to show fft
+blur = abs(fftshift(ifft2(fftshift(ACR))));
 %display image
 figure ()
 image(blur)
-colormap(gray(256));
-% problem here: will need to make this a circle instead of a rectangle,
-% CIRCLE = insertShape(blank,'filled circle',[128 128 8]);   ?
-% moving on...
-
-% Measure Intensity
-
-% Save small image in cell array
-
+colormap(winter(256));
+% problem here: will need to make this a circle instead of a rectangle
+% possible solution to the transfer function:
+% CIRCLE = insertShape(blank,'filled circle',[128 128 8]);
 
