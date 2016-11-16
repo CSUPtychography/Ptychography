5976da3e247951a96cad0503ee4e5b2113c4215c
Robby <robbystk@rams.colostate.edu>
2016-11-15 17:14:27 -0700

    Use circular OTF instead of rectangular

--- mock-data/mock.m
+++ mock-data/mock.m
@@ -91,46 +91,53 @@ end
 % and -0.5 to 0.5 for phase
 mag2 = double(mag) / 255;
 phase2 = double(phase) / 255 - 0.5;
+
 % pack into single phase image
 I = mag2 .* exp(1i * 2 * pi * phase2);
-% conserve memory ('cause we're gonna need it)
+
 %% small image construction
+
 % perform FFT on I
 IF = fftshift(fft2(fftshift(I)));
+
 % preparing variables for transfer function
-k = r+1;
-[m,n] = size(mag);
-Images = cell((size(mag)/r)-1);
+k = r;
+[m,n] = size(I);
+[X,Y] = meshgrid(1:n,1:m);
+Images = cell(floor([m,n] / r - 1));
 imagesX=1;
 while(k < m);
-j=r+1;
-imagesY=1;
+    j=r;
+    imagesY=1;
     while (j < n);
         % preparing transfer function
-    TF = zeros(size(mag));
-    TF(k-r:k+r-1,j-r:j+r-1) = 255;
-        % multiplying transfer function by FFt'd function, and assigning to
-        % cell array
-    Images{imagesX,imagesY} = TF.*IF;
+        OTF = (sqrt((X - k).^2 + (Y - j).^2) < r);
+        OTF(k-r+1:k+r,j-r+1:j+r) = 255;
+        % multiply transfer function by FFt'd image
+        blurred = OTF .* IF;
+        % crop out region of interest
+        blurred = blurred(k-r+1:k+r, j-r+1:j+r);
+        % inverse fourier transform
+        sub_image = fftshift(ifft2(fftshift(blurred)));
+        % measure intensity
+        sub_image = abs(sub_image).^2;
+        % store in cell array
+        Images{imagesX,imagesY} = sub_image;
+
         % counting through the image
-    imagesY = imagesY+1;
-    j = j+r;
-    end
-imagesX=imagesX+1;
-k=k+r;
+        imagesY = imagesY+1;
+        j = j+r;
+    end % while
+    imagesX=imagesX+1;
+    k=k+r;
 end
 %% to be done with each segment
 A = Images{a,b};
-%trim each image
-AC = A(A~=0);
-%reshape after trimming zeros
-ACR = reshape(AC,2*r,2*r);
-%perform ifft2 on fewer pixels to show fft
-blur = abs(fftshift(ifft2(fftshift(ACR))));
+
 %display image
-figure ()
-image(blur)
-colormap(winter(256));
+% figure ()
+image(A)
+colormap gray;
 % problem here: will need to make this a circle instead of a rectangle
 % possible solution to the transfer function:
 % CIRCLE = insertShape(blank,'filled circle',[128 128 8]);
