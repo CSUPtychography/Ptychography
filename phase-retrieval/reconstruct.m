08c267ab367e133fa7dd63c0f3caeb651e6596be
robby <robbystk@gmail.com>
2016-12-28 13:13:11 -0700

    Generate transfer function

--- phase-retrieval/reconstruct.m
+++ phase-retrieval/reconstruct.m
@@ -27,9 +27,18 @@ object = complex(zeros(object_size));
 objectFT = fftshift(fft2(object));
 % interpolate on-axis image maybe?  
 
+% create list of k vectors
+k_x = r:delta_k:object_size(1) - r;
+k_y = r:delta_k:object_size(2) - r;
+
+% grid of indices
+[X,Y] = meshgrid(1:object_size(1),1:object_size(2));
+
 for iter = 1:iterations         % one per iteration
     for i = 1:array_size        % one per row of LEDs
         for j = 1:array_size    % one per column of LEDs
+            % prepare CTF (circle radius R center (k_x,k_y))
+            CTF = ((X - k_x(i)).^2 + (Y - k_y(j)).^2 < r^2);
             % extract piece of spectrum
             
             % FFT
