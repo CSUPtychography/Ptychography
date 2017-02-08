8129c61803bf2c21415812e807686973a64c0e5e
Robby Stokoe <robbystk@rams.colostate.edu>
2017-02-07 20:06:52 -0700

    Allow getting two separate x & y lists or one list

--- mock-data/squiral.m
+++ mock-data/squiral.m
@@ -1,9 +1,13 @@
-function sequence = squiral(length)
+function varargout = squiral(length)
 %squiral: generates a sequence of points in an outward square spiral
-%    usage:  sequence = spiral(length);
+%    usage:  sequence = squiral(length);
+%        or: [x, y] = squiral(length);
 %    input:  the length (number of points) in the spiral 
 %              (choose a square for a square spiral)
-%    output: a 2 x length list of points (x,y)
+%    output: a 2 x length list of points ([x; y])
+%        or: two 1 x length lists of points, one each for x & y
+
+    nargoutchk(0,2);    % check for correct number of outputs
 
     rotate = [0 1; -1 0];
     delta = [1; 0];
@@ -26,5 +30,14 @@ function sequence = squiral(length)
             end % delta if
         end % step if
     end % for
+
+    if (nargout == 1 || nargout == 0)
+        varargout{1} = sequence;
+    elseif (nargout == 2)
+        varargout{1} = sequence(1,:);
+        varargout{2} = sequence(2,:);
+    else
+        error('Wrong number of outputs');
+    end % nargout if
 
 end % function
