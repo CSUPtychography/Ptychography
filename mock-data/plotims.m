8bddc6dc1abb537ff349c01157459b2f8c8ab978
Robby Stokoe <robbystk@rams.colostate.edu>
2017-02-20 11:59:53 -0700

    Remove axis labels so images are bigger

--- mock-data/plotims.m
+++ mock-data/plotims.m
@@ -7,8 +7,8 @@ for i = 1:Ni
     for j = 1:Nj
         subplot(Ni,Nj,j+Ni*(i-1));
         imagesc(abs(images{i,j}))
+        set(gca, 'YTickLabel', [], 'XTickLabel', []);
         title(sprintf('image %d, %d',i,j))
         axis image;
     end %for
 end %for
-    
