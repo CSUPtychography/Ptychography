b8cc39cb8a7369aeafd9a7087173bfc0f404d012
Nick Brown <nckbrwn@rams.colostate.edu>
2017-03-05 12:20:15 -0700

    Create Matlab Function that returns the images, also verifies camera and Led Control

---
+++ LEDControl/LCchat.m
@@ -0,0 +1,16 @@
+function [allimages] = LCchat(arduino,x,xmax,y,ymax,passes)
+%calculate how high I should go(maybe do this in arduino code and pass to
+%here instead through serial)
+%%j = (xmax-x)*(ymax-y)*passes;
+j = 100;
+for i = 1:j
+    out = serialpass(arduino,trigger,x,y); %returns whether or not the LED was triggered
+    if out == 1                            %test if Led was triggered
+        image(x,y) = takephoto(res,exp);        %returns the image for array x,y
+    else
+        i = i-1;                            %re-triggers the LED to switch
+    end
+    x = x+1;
+    y = y+1;
+end
+end
