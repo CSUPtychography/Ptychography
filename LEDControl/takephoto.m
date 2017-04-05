d72ed7ad7d52025bea9f54cfedbdb25a172afbdd
Nicholas <nckbrwn@rams.colostate.edu>
2017-04-05 14:42:19 -0600

    Add Actual Image Acquisition, Scale to exposure

--- LEDControl/takephoto.m
+++ LEDControl/takephoto.m
@@ -1,10 +1,13 @@
-function[image] = takephoto(res,exp)
-
-
-
-
-
-mbox = msgbox('Camera took a photo with res and exp'); 
-uiwait(mbox);
-image = 1;
+function[image] = takephoto(vid,vidsrc)
+%prev = preview(vid);
+%mbox_preview = msgbox('Preview looks good, click to take photo');
+%uiwait(mbox_preview);
+image_raw = getsnapshot(vid);
+exposure = vidsrc.ExposureTimeAbs;
+double_image = double(image_raw);
+scaled_double_image = double_image./255;
+exp_scaled_double_image = scaled_double_image./exposure;
+%mbox_image = msgbox('Camera took a photo,scaled it, and stored it');
+%uiwait(mbox_image);
+image = exp_scaled_double_image;
 end
