3c19716ed142ef13f5f47f2939fada422bc743ca
Nicholas <nckbrwn@rams.colostate.edu>
2017-04-05 14:35:32 -0600

    Create file to setup camera

--- LEDControl/camerasetup.m
+++ LEDControl/camerasetup.m
@@ -1,8 +1,13 @@
-function [cam,Flag]=camerasetup
+function [vid,vidsrc,Flag]=camerasetup()
 % this function sets up the drivers, finds all connected cameras, sets up
 % the camera, and returns the camera information to the main code
-make();
-available_cams = baslerFindCameras();
-cameraIndex = available_cams(1,1);
-cam = baslerCameraInfo(cameraIndex);
+a = imaqhwinfo;
+CON = char(a.InstalledAdaptors);
+vid = videoinput(CON);
+vidsrc = getselectedsource(vid);
+%% anything down here can be changed, optional parameters in comments
+vidsrc.ExposureAuto = 'Continuous'; %'Off', 'Once', 'Continuous'
+Flag = 1;
+mbox_cam = msgbox('Camera setup');
+uiwait(mbox)
 end
