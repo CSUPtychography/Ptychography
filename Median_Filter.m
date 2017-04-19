435529c182ec8f60d35bac7e62ebb090f5bd653b
Nicholas <nckbrwn@rams.colostate.edu>
2017-04-19 13:35:00 -0600

    Fix more bugs that occured when moving to reconstruct the filtered image

--- Median_Filter.m
+++ Median_Filter.m
@@ -2,20 +2,20 @@ function MFOBJECT = Median_Filter(directory)
 newdirectory = datestr(now,'yyyy-mm-dd_HH-MM-SS');
 mkdir(newdirectory);
 oldfileformat = 'image-x%d_y%d';
-newfileformat = 'F-image-x%d_y%d';
+newfileformat = 'image-x%d_y%d';
 oldfullformat = strcat(newdirectory,'/',oldfileformat);
 newfullformat = strcat(newdirectory,'/',newfileformat);
 for i = 1:15
     for j = 1:15
 load(fullfile(directory,sprintf(oldfileformat,i,j)));
 prefiltered = Image;
-postfiltered = medfilt2(prefiltered);
-save(sprintf(newfullformat,i,j),'postfiltered');
+Image = medfilt2(prefiltered);
+save(sprintf(newfullformat,i,j),'Image');
 figure(1)
 subplot(1,2,1)
 imagesc(prefiltered)
 subplot(1,2,2)
-imagesc(postfiltered)
+imagesc(Image)
 pause(.0001)
     end
 end
