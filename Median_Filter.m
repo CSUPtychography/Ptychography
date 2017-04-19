32b1a75657a9d23f8a05159093aa87b0f0c9040b
Nicholas <nckbrwn@rams.colostate.edu>
2017-04-19 12:58:08 -0600

    Fix Initial bugs/incorrect coding

--- Median_Filter.m
+++ Median_Filter.m
@@ -1,13 +1,15 @@
-function MFOBJECT = MedianFilter(directory)
+function MFOBJECT = Median_Filter(directory)
 newdirectory = datestr(now,'yyyy-mm-dd_HH-MM-SS');
 mkdir(newdirectory);
-fileformat = 'F-image-x%d_y%d';
-fullformat = strcat(newdirectory,'/',fileformat);
+oldfileformat = 'image-x%d_y%d';
+newfileformat = 'F-image-x%d_y%d';
+oldfullformat = strcat(newdirectory,'/',oldfileformat);
+newfullformat = strcat(newdirectory,'/',newfileformat);
 for i = 1:15
     for j = 1:15
-prefiltered = load(fullfile(directory,sprintf(fileformat,i,j));
-postfiltered = medfilt2(prefilitered);
-save(sprintf(fullformat,ImageArrayX,ImageArrayY),'postfiltered');
+prefiltered = load(fullfile(directory,sprintf(newfileformat,i,j)));
+postfiltered = medfilt2(prefiltered);
+save(sprintf(newfullformat,ImageArrayX,ImageArrayY),'postfiltered');
 figure(1)
 subplot(1,2,1)
 imagesc(prefiltered)
