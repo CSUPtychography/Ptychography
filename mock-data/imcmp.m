d0b265aa466ff80495dfc97195ab6d3988c1b294
Robby Stokoe <robbystk@rams.colostate.edu>
2017-05-06 15:23:07 -0600

    Create function to compare images

---
+++ mock-data/imcmp.m
@@ -0,0 +1,23 @@
+function similarity = imcmp(image,original);
+%imcmp: compare two images irrespective of size, scale, or offset using
+%        the Pearson correlation coefficient
+%    inputs: image:      The image to compare
+%            original:   The image to compare it against
+%    output: similarity: The quantitative similarity between the images
+
+    image = imresize(image,size(original));
+
+    image = image(:);
+    original = original(:);
+
+    im_mean = mean(image);
+    orig_mean = mean(original);
+
+    im_std = std(image);
+    orig_std = std(image);
+
+    covariance = mean(image.*original) - im_mean*orig_mean;
+
+    similarity = covariance / im_std / orig_std;
+
+end % imcmp
